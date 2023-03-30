d8981c25f08a1264c43aca6d5cbc7037ad47dad5
Ari <128650932+AriHid11@users.noreply.github.com>
2023-03-30 20:21:43 -0300

    main1

--- OneDrive/Documentos/NetBeansProjects/Main/src/main/Main.java
+++ OneDrive/Documentos/NetBeansProjects/Main/src/main/Main.java
@@ -1,13 +1,23 @@
 
-package main;
-
+package Clases_y_Objetos;
+import java.util.*;
 public class Main {
-
-   
-   
+  
     public static void main(String[] args) {
-        System.out.println("hola");
-        
+     Scanner sc =new Scanner(System.in);
+     int num; 
+     boolean longitud= true;
+     int c=1;
+        System.out.println("cargar el numero a definir");
+     num=sc.nextInt();
+     while(longitud){
+        if(num/10!=0){
+            num= num/10;
+            c++;
+        }else{
+            longitud=false;
+        }
     }
-    
+        System.out.println("El numero ingresado tiene una longitud de: "+c);
+    }
 }
